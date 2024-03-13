99bae8bd92af39ac4de1dcfbb7f18a0ba6f720d3
landry kankeu <118926620+KFLandry@users.noreply.github.com>
2024-03-13 12:19:13 +0100

    ...

--- src/public/ressource/secret.js
+++ src/public/ressource/secret.js
@@ -4,8 +4,8 @@ export const APITOKEN = "9a708c77-7e1f-4c44-b06a-2a67abee80e7"
 export const EMAILTEST = 'kankeulandry26@gmail.com'
 // 
 export const DOMAINFRONT = "http://127.0.0.1:5502"
-export const DOMAINBACK = "http://localhost:57910"
-// export const DOMAINBACK = "https://educycle-d5986f362a7b.herokuapp.com/index.php"
+// export const DOMAINBACK = "http://localhost:57910"
+export const DOMAINBACK = "https://educycle-d5986f362a7b.herokuapp.com/index.php"
 // export const DOMAINFRONT = "https://educyclefront-07f9d84796fd.herokuapp.com"
 // Aire de Recherche
 export const LATITTUDE = 43.4211
