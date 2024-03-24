8738baa6ca42c15ea42daea1284422ed104a9dc1
landry kankeu <118926620+KFLandry@users.noreply.github.com>
2024-03-24 19:45:12 +0100

    Try to run on Platform.sh

--- src/public/ressource/secret.js
+++ src/public/ressource/secret.js
@@ -4,7 +4,7 @@ export const APITOKEN = "9a708c77-7e1f-4c44-b06a-2a67abee80e7"
 export const EMAILTEST = 'kankeulandry26@gmail.com'
 // 
 export const DOMAINFRONT = "http://127.0.0.1:3000"
-export const DOMAINBACK = "http://localhost:62164"
+export const DOMAINBACK = "http://localhost:62514"
 // export const DOMAINBACK = "https://educycle-d5986f362a7b.herokuapp.com/index.php"
 // export const DOMAINFRONT =  "https://educyclefront-07f9d84796fd.herokuapp.com"
 // Aire de Recherche
